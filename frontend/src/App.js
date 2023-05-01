eb9eafa70a23598f327e1bfd1e30ceb68a4eec00
faizyabShah <faizyabshah0123@gmail.com>
2023-05-01 13:34:15 +0500

    Registerfile added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Navbar from "./components/Navbar"
-import Login from './pages/Login'
-import './App.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./App.scss";
 
 function App() {
-
-
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar links={[{path: '/about', name: 'About'}, {path: './login', name: 'Login'}]} />
-        <div className='pages'>
+        <Navbar
+          links={[
+            { path: "/about", name: "About" },
+            { path: "./login", name: "Login" },
+            { path: "./register", name: "Register" },
+          ]}
+        />
+        <div className="pages">
           <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} ></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
           </Routes>
         </div>
       </BrowserRouter>
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,7 @@
+import "./Register.scss";
+
+const Register = function () {
+  return <div>Register</div>;
+};
+
+export default Register;
