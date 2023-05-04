5ee4f289a86466ecbb9cb239a3c0b8ceb53c49fb
faizyabShah <faizyabshah0123@gmail.com>
2023-05-04 20:28:52 +0500

    Registration page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,90 @@
 import "./Register.scss";
+import { useState } from "react";
 
 const Register = function () {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [contact, setContact] = useState("");
+  const [location, setLocation] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = { name, password, contact, location, email };
+
+    console.log(user);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="contact">Contact</label>
+          <input
+            type="number"
+            className="form-control"
+            id="contact"
+            placeholder="Enter contact"
+            value={contact}
+            onChange={(e) => setContact(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="location">Location</label>
+          <input
+            type="text"
+            className="form-control"
+            id="location"
+            placeholder="Enter location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
