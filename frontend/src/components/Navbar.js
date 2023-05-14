e7308f9cd0e5c36163ab7166d85b86b8f99ddeb1
just-curious12 <manahil1078@hotmail.com>
2023-05-14 17:18:35 +0500

    +navbar scss

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -48,3 +48,54 @@ const Navbar = function ({ links }) {
 };
 
 export default Navbar;
+// import { Navbar, Nav } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { useUserContext } from "../hooks/userContextHook";
+// import { logout } from "../actions/user";
+// import { orgLogout } from "../actions/org";
+
+// function MyNavbar({ links }) {
+//   const { isOrg, isLoggedIn, user, dispatch } = useUserContext();
+
+//   const handleLogout = () => {
+//     if (!isOrg) {
+//       logout(dispatch);
+//     } else {
+//       orgLogout(dispatch);
+//     }
+//   };
+
+//   return (
+//     <Navbar bg="light" expand="lg">
+//       <Link to={isOrg ? "/org-dashboard" : "/"} className="navbar-brand">
+//         <h1>Home</h1>
+//       </Link>
+//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//       <Navbar.Collapse id="basic-navbar-nav">
+//         <Nav className="mr-auto">
+//           {links.map((link) =>
+//             link ? (
+//               <Nav.Link key={link.name} as={Link} to={link.path}>
+//                 <h3>{link.name}</h3>
+//               </Nav.Link>
+//             ) : (
+//               <></>
+//             )
+//           )}
+//         </Nav>
+//         {isLoggedIn ? (
+//           <Nav>
+//             <Nav.Link as={Link} to="/profile">
+//               <h3>{user.name}</h3>
+//             </Nav.Link>
+//             <Nav.Link onClick={handleLogout}>
+//               <h3>Logout</h3>
+//             </Nav.Link>
+//           </Nav>
+//         ) : null}
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
+
+// export default MyNavbar;
